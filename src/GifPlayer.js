8e8160df15f825a025729cffd26a86b51a60c61d
Ben Wiley <therealbenwiley@gmail.com>
2016-10-26 01:40:29 -0400

    GifPlayerContainer manages playing state of GifPlayer.

---
+++ src/GifPlayer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import './GifPlayer.scss';
+
+const GifPlayer = ({ gif, still, playing, toggle, ...rest }) => (
+  <div
+    className={classNames('gif_player', { 'playing': playing })}
+    onClick={toggle}
+  >
+    <div className='play_button' />
+    <img {...rest} src={playing ? gif : still} />
+  </div>
+);
+
+GifPlayer.propTypes = {
+  gif: React.PropTypes.string,
+  still: React.PropTypes.string,
+  playing: React.PropTypes.bool,
+  toggle: React.PropTypes.func
+};
+
+export default GifPlayer;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,50 @@
 import React from 'react';
-import classNames from 'classnames';
 
-import './GifPlayer.scss';
+import GifPlayer from './GifPlayer';
 
-const GifPlayer = ({ gif, still, playing, ...rest }) => (
-  <div className={classNames('gif_player', { 'playing': playing })}>
-    <div className='play_button' />
-    <img {...rest} src={playing ? gif : still} />
-  </div>
-);
+const preloadGif = src => {
+  new Image().src = src;
+};
 
-export default GifPlayer;
+class GifPlayerContainer extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      playing: false
+    };
+    if (props.gif) {
+      preloadGif(props.gif);
+    }
+  }
+
+  componentDidMount (nextProps) {
+    const oldGif = this.props.gif;
+    const newGif = nextProps.gif;
+    if (newGif && oldGif !== newGif) {
+      preloadGif(newGif);
+    }
+  }
+
+  toggle () {
+    this.setState({
+      playing: !this.state.playing
+    });
+  }
+
+  render () {
+    return (
+      <GifPlayer
+        {...this.props}
+        playing={this.state.playing}
+        toggle={() => this.toggle()}
+      />
+    );
+  }
+}
+
+GifPlayerContainer.propTypes = {
+  gif: React.PropTypes.string,
+  still: React.PropTypes.string
+};
+
+export default GifPlayerContainer;
